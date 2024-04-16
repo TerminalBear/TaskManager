9bbf1c915c16d9ea6ad0a2a3ce8c717a2359072b
nomad1712 <123312290+nomad1712@users.noreply.github.com>
2024-04-16 19:21:05 -0400

    Editing hash table

--- HashTable.java
+++ HashTable.java
@@ -16,11 +16,15 @@ public class HashTable {
             table[i] = new LinkedList<>();
         }
     }
-
-    private int hash(int priority) {
-        return priority % size;
-    }
-
+//altered the hash function to distributly more uniformly 
+private int hash(int priority) {
+    int hash = 0;
+    hash += priority;
+    hash += (priority >> 10) ^ 0x12345678;
+    hash += (priority >> 20) ^ 0x87654321;
+    hash += (priority >> 30) ^ 0x55555555;
+    return Math.abs(hash % size);
+}
     public int currSize() {
         return this.currSize;
     }
@@ -63,8 +67,10 @@ public class HashTable {
             return false;
         }
     }
+    
 
     @SuppressWarnings("unchecked")
+    //resize function altered to 
     private void resize() {
         size = size * 2;
         LinkedList<Task>[] oldTable = table;
@@ -75,8 +81,88 @@ public class HashTable {
         entries = 0;
         for (LinkedList<Task> bucket : oldTable) {
             for (Task task : bucket) {
-                put(task);
+                int index = hash(task.getUserSetPriority());
+                table[index].add(task);
+                entries++;
             }
         }
+}}
+// potential change using an array.
+/*private Task[] table;
+
+public HashTable(int size, float loadFactor) {
+    this.size = size;
+    this.loadFactor = loadFactor;
+    table = new Task[size];
+    for (int i = 0; i < size; i++) {
+        table[i] = null;
     }
 }
+
+private int hash(int priority) {
+    return priority % size;
+}
+
+public void put(Task task) {
+    if (entries >= size * loadFactor) {
+        resize();
+    }
+    int index = hash(task.getUserSetPriority());
+    if (table[index] == null) {
+        table[index] = new Task[1];
+    }
+    table[index][0] = task;
+    entries++;
+    this.currSize++;
+}
+
+public Task get(int priority) {
+    int index = hash(priority);
+    if (table[index] == null) {
+        return null;
+    }
+    return table[index][0];
+}
+
+public boolean remove(Task task) {
+    int index = hash(task.getUserSetPriority());
+    if (table[index] == null) {
+        return false;
+    }
+    table[index] = null;
+    this.currSize--;
+    return true;
+}
+
+public boolean contains(Task task) {
+    int index = hash(task.getUserSetPriority());
+    if (table[index] == null) {
+        return false;
+    }
+    return table[index][0].equals(task);
+}
+
+public boolean updatePriority(Task task, int newPriority) {
+    if (remove(task)) {
+        task.setUserSetPriority(newPriority);
+        put(task);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+private void resize() {
+    size = size * 2;
+    Task[] oldTable = table;
+    table = new Task[size];
+    for (int i = 0; i < size; i++) {
+        table[i] = null;
+    }
+    entries = 0;
+    for (Task task : oldTable) {
+        if (task!= null) {
+            put(task);
+        }
+    }
+} */
